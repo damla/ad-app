f25f726e0ddf7073babbe301082fba3add90e734
Damla Koksal <me@damlakoksal.com>
2023-09-12 12:23:24 +0300

    refactor[#8]: applied on error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Button from '@/components/general/button/button'
+import { useState } from 'react'
 
 interface Props {
   error: Error
@@ -8,10 +9,19 @@ interface Props {
 }
 
 const Error: React.FC<Props> = ({ error, reset }) => {
+  const [show, setShow] = useState(false)
+  const showError = () => {
+    setShow(true)
+  }
+
   return (
     <div className='textCenter'>
       <h2 className='mb3xl'>Hata oluştu!</h2>
       <Button onClick={reset}>Tekrar Dene</Button>
+      <Button onClick={showError} className='ml2xl'>
+        Hatayı Göster
+      </Button>
+      {show && <pre className='mt3xl pre'>{error.message}</pre>}
     </div>
   )
 }
